28b851438b9164014dd582655af60c6550fb9d6c
Ritesh Mishra <riteshmishra9565@gmail.com>
2026-08-08 20:26:17 +0530

    fix: pointer events none add

--- src/components/core/Home/how-its-work/Card.tsx
+++ src/components/core/Home/how-its-work/Card.tsx
@@ -13,7 +13,7 @@ const Card = ({ id, title, description, image }: CardProps) => {
 
   return (
     <article
-      className={`group shadow shadow-gray-600 flex h-full flex-col rounded-2xl border border-white/40 bg-white/5 p-6 backdrop-blur-xl transition-transform duration-300 hover:-translate-y-1 ${
+      className={`select-none group shadow shadow-gray-600 flex h-full flex-col rounded-2xl border border-white/40 bg-white/5 p-6 backdrop-blur-xl transition-transform duration-300 hover:-translate-y-1 ${
         isOffset ? "lg:translate-y-8" : ""
       }`}
     >
@@ -27,14 +27,14 @@ const Card = ({ id, title, description, image }: CardProps) => {
       </div>
 
       {/* Image */}
-      <div className="relative mb-6 aspect-square overflow-hidden rounded-xl border border-white/5">
+      <div className="relative mb-6 aspect-square overflow-hidden rounded-xl border border-white/5 ">
         <Image
           src={image}
           alt={title}
           title={`${title} image`}
           fill
           sizes="(max-width: 1024px) 50vw, 25vw"
-          className="object-cover opacity-80 transition-all duration-500 group-hover:scale-105 group-hover:opacity-100 active:opacity-100"
+          className="object-cover opacity-80 transition-all duration-500 group-hover:scale-105 group-hover:opacity-100 active:opacity-100 pointer-events-none"
         />
       </div>
 
