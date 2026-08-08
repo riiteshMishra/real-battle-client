2ca87ab1db55cfc1dda645c75aa1195f1aa41bff
Ritesh Mishra <riteshmishra9565@gmail.com>
2026-08-08 18:16:42 +0530

    fix: In hero section heading changed feature-future

--- src/components/core/Home/Hero/Heading.tsx
+++ src/components/core/Home/Hero/Heading.tsx
@@ -1,7 +1,7 @@
 const Heading = () => {
   return (
     <h3 className="text-4xl lg:text-6xl w-80 sm:w-auto capitalize mx-auto text-center font-sekuya  font-bold">
-      The feature of competitive{" "}
+      The future of competitive{" "}
       <span className="text-transparent bg-clip-text bg-linear-to-r from-purple-500 via-cyan-400 to-orange-500">
         <strong>gaming !</strong>
       </span>
