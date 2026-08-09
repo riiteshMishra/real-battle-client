3fda0593c75adcc87df4be663afa2fcb0824d69d
Ritesh Mishra <riteshmishra9565@gmail.com>
2026-08-09 16:11:57 +0530

    refactor: update how it works section

--- src/components/core/Home/how-its-work/Index.tsx
+++ src/components/core/Home/how-its-work/Index.tsx
@@ -5,11 +5,11 @@ import Cards from "./Cards";
 
 const HowItsWork = () => {
   return (
-    <section id="how-it-works" className="space-y-10">
+    <section id="how-it-works" className="py-10 md:py-18">
       <Heading />
       <SubHeading
         title={homeData.subHeadin3}
-        className="text-center capitalize text-sm sm:text max-w-100 mx-auto"
+        className="text-center capitalize text-sm sm:text max-w-100 mx-auto my-10"
       />
       <div className="max-w-40 bg-primary h-0.5 mx-auto rounded-ful" />
       <Cards />
