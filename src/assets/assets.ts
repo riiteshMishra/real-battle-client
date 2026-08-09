557967b1a52649875b87673984b1a130c6bf9bee
Ritesh Mishra <riteshmishra9565@gmail.com>
2026-08-09 16:12:25 +0530

    feat: register supported game assets

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -4,6 +4,9 @@ import ChooseYourTour from "./how-it-works/choose-your-tournament.jpg";
 import JoinAndCompete from "./how-it-works/join-and-compete.jpg";
 import PlayAndGetResult from "./how-it-works/play-and-get-result.jpg";
 import WinAndGetRewarded from "./how-it-works/win-and-get-rewarded.jpg";
+import FreeFire from "./games/free-fire.png";
+import BGMI from "./games/bgmi.png";
+import COD from "./games/cod.png";
 
 export const assets = {
   HeroImage,
@@ -12,4 +15,7 @@ export const assets = {
   JoinAndCompete,
   PlayAndGetResult,
   WinAndGetRewarded,
+  FreeFire,
+  BGMI,
+  COD,
 };
