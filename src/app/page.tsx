2a72bc8b9dec04112842264928da4d1ffd5303d8
Ritesh Mishra <riteshmishra9565@gmail.com>
2026-08-09 16:11:49 +0530

    feat: integrate supported games section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Features from "../components/core/Home/Features/Index";
 import HeroSection from "../components/core/Home/Hero/HeroSection";
 import HowItsWork from "../components/core/Home/how-its-work/Index";
 import Stats from "../components/core/Home/stats/Index";
+import SupportedGames from "../components/core/Home/supported-games/Index";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Features />
       <HowItsWork />
       <Stats />
+      <SupportedGames />
     </section>
   );
 };
