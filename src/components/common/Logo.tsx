07a99f71940935a477929ee406a84fffcdadc5aa
Ritesh Mishra <riteshmishra9565@gmail.com>
2026-08-10 15:15:36 +0530

    refactor: update Logo component

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,8 +1,12 @@
-const Logo = () => {
+interface LogoProps {
+  className: string;
+}
+
+const Logo = ({ className }: LogoProps) => {
   return (
     <span
       title="platform-name"
-      className="text-transparent bg-clip-text bg-linear-to-r from-purple-500 via-cyan-400 to-orange-500"
+      className={`text-transparent bg-clip-text bg-linear-to-r from-purple-500 via-cyan-400 to-orange-500 ${className}   `}
     >
       <strong>Real Battle</strong>
     </span>
