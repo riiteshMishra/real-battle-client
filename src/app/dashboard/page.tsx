cda61a48579ac43a7f2798d7440d04714618a057
Ritesh Mishra <riteshmishra9565@gmail.com>
2026-08-18 20:47:26 +0530

    feat: add dashboard page
    - Create dashboard layout
    - Add user dashboard components

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      dashboard Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Laudantium, assumenda? Praesentium totam necessitatibus placeat
+      reprehenderit quos officia, aut exercitationem, eum alias consectetur
+      adipisci accusantium dicta magni aliquid quidem ullam culpa tempora nam
+      natus cum. Ratione mollitia, aspernatur quam molestiae culpa voluptatem
+      dolorum eum repudiandae nemo impedit quia reiciendis deleniti laudantium.
+      Dolorum fuga et exercitationem, nisi, aliquid id illum delectus veniam
+      illo, assumenda inventore. Nostrum illum dicta, nisi repellat eum modi
+      expedita mollitia doloribus quis ipsum, iste excepturi consectetur sunt!
+      Tempore qui suscipit iusto omnis, ratione ab laudantium dicta. Repellendus
+      illum mollitia in tempore labore eligendi maiores non architecto corporis
+      recusandae.
+    </div>
+  );
+};
+
+export default page;
