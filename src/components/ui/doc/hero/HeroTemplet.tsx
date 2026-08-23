91e8d121fc0f1853bd224c0f035789729ee025e9
Ritesh Mishra <riteshmishra9565@gmail.com>
2026-08-23 11:40:07 +0530

    chore: add prettier config and shadcn/ui components setup

---
+++ src/components/ui/doc/hero/HeroTemplet.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const HeroTemplet = () => {
+  return (
+    <section className="relative isolate overflow-hidden bg-linear-to-br from-indigo-600 via-blue-700 to-violet-800 text-white dark:from-slate-950 dark:via-indigo-950 dark:to-slate-950"></section>
+  );
+};
+
+export default HeroTemplet;
